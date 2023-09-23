5b09b9274e5478fd793cb13710d013860b1aade5
kadri.mannimets <kadri.mannimets@cgi.com>
2023-09-23 10:21:36 +0300

    Create Spring backend.

---
+++ mushrooms-app/src/main/java/com/testassignment/mushroomsapp/MushroomsAppApplication.java
@@ -0,0 +1,13 @@
+package com.testassignment.mushroomsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MushroomsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MushroomsAppApplication.class, args);
+    }
+
+}
