7526873ab992e2f078558bce56a6dae20a8a64ea
kadri.mannimets <kadri.mannimets@cgi.com>
2023-09-27 10:45:29 +0300

    Add some test data.

---
+++ mushrooms-app/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO mushroom_point (name, lat, lng) VALUES ('chanterelle', 59.034902, 24.763184);
+INSERT INTO mushroom_point (name, details, lat, lng) VALUES ('boletus', 'Watch out for snakes!', 58.505175, 25.543213);
+INSERT INTO mushroom_point (name, details, lat, lng) VALUES ('chanterelle', 'There is also a nice river there.', 58.257508, 25.64209);
+INSERT INTO mushroom_point (name, lat, lng) VALUES ('chanterelle', 57.926517, 26.982422);
+INSERT INTO mushroom_point (name, details, lat, lng) VALUES ('fly agaric', 'Pretty. But do not eat - poisonous!', 59.170298, 26.806641);
+INSERT INTO mushroom_point (name, details, lat, lng) VALUES ('fly agaric', 'Lots of wild animals, be careful!', 59.070298, 24.806641);
---
+++ mushrooms-app/src/test/java/com/testassignment/mushroomsapp/MushroomsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testassignment.mushroomsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MushroomsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
